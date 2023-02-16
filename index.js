180c4fd8e52c22027779ad9e162187dc8a0ea664
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-02-16 21:21:40 +0100

    made a squadpage, minimal styling

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-// Importeer express uit de node_modules map
-import express from "express";
+import express from 'express'
 
-const url = "https://whois.fdnd.nl/api/v1/member/ivar-schuyt";
-const data = await fetch(url).then((response) => response.json());
+const url = 'https://whois.fdnd.nl/api/v1/squad/'
 
-// Maak een nieuwe express app aan
-const app = express();
+// Maak een nieuwe express app
+const app = express()
 
-// Stel ejs in als template engine en geef de 'views' map door
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
-// Gebruik de map 'public' voor statische resources
-app.use(express.static("public"));
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
 
 // Maak een route voor de index
-app.get("/", function (req, res) {
-  // res.send('Hello World!')
-  res.render("index", data);
-});
+app.get('/', (request, response) => {
+  console.log(request.query.squad)
 
-// Stel het poortnummer in waar express op gaat luisteren
-app.set("port", process.env.PORT || 8000);
+  let slug = request.query.squad || 'squad-b-2022'
+  let orderBy = request.query.orderBy || 'name'
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
 
-// Start express op, haal het ingestelde poortnummer op
-app.listen(app.get("port"), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get("port")}`);
-});
+  fetchJson(squadUrl).then((data) => {
+    response.render('index', data)
+  })
+})
+
+// app.get('/members', (request, response) => {
+//   response.send('Joepie!!')
+// })
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
